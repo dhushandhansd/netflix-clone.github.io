281c893e4e6aa4ff68cacb76e25e20e84565056a
ShreeDhushandhan <dhushandhan.sd@gmail.com>
2021-08-12 17:20:10 +0530

    Background Image File path changed, Select Options removed and function modified

--- assets/script/navbarFunctions.js
+++ assets/script/navbarFunctions.js
@@ -8,12 +8,12 @@ function langSelection() {
 }
 
 if(localStorage.getItem('language') === 'English') {
-  selectIndex = 1;
+  selectIndex = 0;
   langSelect.selectedIndex = selectIndex;
 } else if(localStorage.getItem('language') === 'Tamil') {
-  selectIndex = 2;
+  selectIndex = 1;
   langSelect.selectedIndex = selectIndex;
 } else if(localStorage.getItem('language') === 'Hindi') {
-  selectIndex = 3;
+  selectIndex = 2;
   langSelect.selectedIndex = selectIndex;
 }
