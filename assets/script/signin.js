7cff52f6f95d0d56faa32cff4d3731d2589b0e01
ShreeDhushandhan <dhushandhan.sd@gmail.com>
2021-08-13 21:27:23 +0530

    Custom Scrollbar Added

--- assets/script/signin.js
+++ assets/script/signin.js
@@ -6,7 +6,7 @@ let signInButton = document.querySelector('#sign-in-btn')
 let invalidEmailText = document.querySelector('.invalid-email-text')
 let invalidPasswordText = document.querySelector('.invalid-password-text')
 
-// localStorage.setItem('authorized',false)
+
 document.cookie = 'authorized=false'
 
 signInButton.addEventListener('click', () => {
@@ -17,7 +17,7 @@ signInButton.addEventListener('click', () => {
     invalidPasswordText.innerHTML = 'Your password must contain between 4 to 60 characters'
     invalidPasswordText.style.display = 'block'
     document.cookie = 'authorized=false'
-    // localStorage.setItem('authorized',false)
+    
   } else {
     invalidEmailText.style.display = 'none'
     invalidPasswordText.style.display = 'none'
@@ -27,14 +27,13 @@ signInButton.addEventListener('click', () => {
     invalidEmailText.style.display = 'none'
     invalidPasswordText.style.display = 'none'
     document.cookie = 'authorized=true'
-    // localStorage.setItem('authorized',true)
     location.href = 'https://dhushandhansd.github.io/netflix-clone.github.io/dashboard.html'
   } else {
     invalidEmailText.innerHTML = 'Invalid Email or Phone Number'
     invalidEmailText.style.display = 'block'
     invalidPasswordText.innerHTML = 'Password does not Match'
     invalidPasswordText.style.display = 'block'
-    // localStorage.setItem('authorized',false)
+    
     document.cookie = 'authorized=true'
   }
 })
