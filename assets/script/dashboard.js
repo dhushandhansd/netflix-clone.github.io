9d42c20773e7dc47338fed453389e1be08f23723
ShreeDhushandhan <dhushandhan.sd@gmail.com>
2021-08-13 18:41:56 +0530

    window.push is not a function fixed and background.css path modified

--- assets/script/dashboard.js
+++ assets/script/dashboard.js
@@ -2,6 +2,6 @@ let auth = localStorage.getItem('authorized')
 
 function authorized() {
   if(auth === 'false') {
-    window.push('https://dhushandhansd.github.io/netflix-clone.github.io/404.html','_self')
+    location.href = 'https://dhushandhansd.github.io/netflix-clone.github.io/404.html'
   }
 }
