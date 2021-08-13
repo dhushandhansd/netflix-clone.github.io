7c1c56f4c2c8ac4937726a655604f3eb175a41c1
ShreeDhushandhan <dhushandhan.sd@gmail.com>
2021-08-13 18:21:51 +0530

    Dashboard with Auth Function added, 404 Page added with styles

---
+++ assets/script/dashboard.js
@@ -0,0 +1,7 @@
+let auth = localStorage.getItem('authorized')
+
+function authorized() {
+  if(auth == false) {
+    window.push('https://dhushandhansd.github.io/netflix-clone.github.io/404.html','_self')
+  }
+}
--- assets/script/signin.js
+++ assets/script/signin.js
@@ -6,6 +6,8 @@ let signInButton = document.querySelector('#sign-in-btn')
 let invalidEmailText = document.querySelector('.invalid-email-text')
 let invalidPasswordText = document.querySelector('.invalid-password-text')
 
+localStorage.setItem('authorized',false)
+
 signInButton.addEventListener('click', () => {
   if(emailInput.value === '' || emailInput.value === null &&
     passwordInput.value === '' || passwordInput.value === null) {
@@ -13,8 +15,22 @@ signInButton.addEventListener('click', () => {
     invalidEmailText.style.display = 'block'
     invalidPasswordText.innerHTML = 'Your password must contain between 4 to 60 characters'
     invalidPasswordText.style.display = 'block'
-  }  else {
+    localStorage.setItem('authorized',false)
+  } else {
     invalidEmailText.style.display = 'none'
     invalidPasswordText.style.display = 'none'
+  }
+  
+  if(emailInput.value == 'dhushandhan.sd@gmail.com' && passwordInput.value == 'shree@123') {
+    invalidEmailText.style.display = 'none'
+    invalidPasswordText.style.display = 'none'
+    localStorage.setItem('authorized',true)
+    location.href = 'https://dhushandhansd.github.io/netflix-clone.github.io/dashboard.html'
+  } else {
+    invalidEmailText.innerHTML = 'Invalid Email or Phone Number'
+    invalidEmailText.style.display = 'block'
+    invalidPasswordText.innerHTML = 'Password does not Match'
+    invalidPasswordText.style.display = 'block'
+    localStorage.setItem('authorized',false)
   }
 })
